66bb70b53f434bfa61e0d5f7448d0f965b3bdada
Yug Marwaha <yugmarwaha987@gmail.com>
2026-02-09 21:34:28 -0600

    chore: remove virtual environment dependencies.

--- backend/venv/lib/python3.9/site-packages/pinecone/admin/resources/__init__.py
+++
@@ -1,4 +0,0 @@
-from .project import ProjectResource
-from .api_key import ApiKeyResource
-
-__all__ = ["ProjectResource", "ApiKeyResource"]
--- backend/venv/lib/python3.9/site-packages/pinecone_plugins/assistant/models/core/dataclass.py
+++
@@ -1,9 +0,0 @@
-from dataclasses import asdict, dataclass
-
-from pinecone_plugins.assistant.models.core.dict_mixin import DictMixin
-
-
-@dataclass
-class BaseDataclass(DictMixin):
-    def to_dict(self):
-        return asdict(self)
